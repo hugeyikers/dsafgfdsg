1028c5ac389afe40a97e49ed872019b3df3e84d6
Adrian Skamarski <147138078+hugeyikers@users.noreply.github.com>
2026-04-15 19:23:54 +0200

    Delete backend directory

--- backend/dist/src/common/interceptors/encryption.interceptor.d.ts
+++
@@ -1,8 +0,0 @@
-import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { EncryptionService } from '../encryption.service';
-export declare class EncryptionInterceptor implements NestInterceptor {
-    private readonly encryptionService;
-    constructor(encryptionService: EncryptionService);
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any>;
-}
--- backend/dist/src/modules/kanban/dto/create-column.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class CreateColumnDto {
-    title: string;
-    limit?: number;
-    color?: string;
-}
